5994ecb350f4e71ebe6d2e58e0c6925206f4018d
3IMAD <dhaimy.imad@gmail.com>
2024-02-09 17:53:41 +0100

    trying vercel analytics knowin im gettin get 2 users

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+        </body>
     </html>
   );
 }
